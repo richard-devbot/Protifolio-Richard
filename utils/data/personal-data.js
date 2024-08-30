e4a3c223fd31ba0167802c79dab90de64aa5e9ad
Richardson Gunde <152559661+richard-devbot@users.noreply.github.com>
2024-08-30 09:47:52 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -14,7 +14,7 @@ export const personalData = {
   email: 'gunderichardson@gmail.com',
   phone: '+91 7569370925',
   address: 'Sri Karthika Manasa Residency, Vizianagarm, Andhra Pradesh - 535003 ',
-  github: 'https://github.com/richard-devbot',
+  github: 'https://github.com/Richardson143',
   facebook: 'https://medium.com/@honeyricky1m3',
   linkedIn: 'https://www.linkedin.com/in/richardson-gunde-589a10249/',
   twitter: 'https://x.com/GundeRicha82508',
