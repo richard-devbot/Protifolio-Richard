e85838f2ba9481ccf3d8d7ce590b3f2a9cfcb3e3
Richardson Gunde <152559661+richard-devbot@users.noreply.github.com>
2024-08-30 09:44:33 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
     email: 'gunderichardson@gmail.com',
     phone: '+91 7569370925',
     address: 'Sri Karthika Manasa Residency, Vizianagarm, Andhra Pradesh - 535003 ',
-    github: 'https://github.com/Richardson143',
+    github: 'https://github.com/richard-devbot',
     medium: 'https://medium.com/@honeyricky1m3',
     linkedIn: 'https://www.linkedin.com/in/richardson-gunde-589a10249/',
     twitter: 'https://x.com/GundeRicha82508',
